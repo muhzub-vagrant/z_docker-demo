3292bce08d98903f6c5a581597c75cc912f8f1e6
muhzub-vagrant <112322082+muhzub-vagrant@users.noreply.github.com>
2022-11-12 14:34:40 +1300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-var server = app.listen(9050, function () {
+var server = app.listen(9950, function () {
   var host = server.address().address;
   var port = server.address().port;
 
